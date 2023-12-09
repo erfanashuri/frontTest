acbd24764bd0edb7d3813de66f475c3543982e10
Erfan <ashoryerfan@yahoo.com>
2023-12-09 17:20:58 +0330

    part #2

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,22 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {});
   });
+
+for (let tab of document.querySelectorAll(".tab")) {
+  tab.addEventListener("click",event=>{
+    document.querySelector(".active").className=document.querySelector(".active").className.replace(" active","")
+    tab.className +=" active"
+    pageChanger(tab.firstChild.innerHTML)
+  })}
+
+function pageChanger(tabName) {
+  if (tabName==="Home") {
+
+    console.log("home");
+
+  }else if(tabName==="Users"){
+    
+    console.log("users");
+
+  }
+}
