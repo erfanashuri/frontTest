19f106c720a9ff2b1889eb5bb84db5a2bc00e5b1
Erfan <ashoryerfan@yahoo.com>
2023-12-31 17:24:40 +0330

    home develop & responsive

--- index.php
+++ index.php
@@ -3,18 +3,17 @@
     <?php include('sidenav.php'); ?>
    <!-- ------------ Content --------------------------------------- -->
    <section class="content-show ">
-      <img src="assets/images/header-img.svg" class="content-img_1 container" alt="Front End">
-    <div class="content-text__container container">
-      <h3>Welcome to Front-End test site...</h3>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit . exahfg Labore molestiae dignissimos asperiores perspiciatis autem consectetur voluptatibus deleniti ex.</p>
-      
-    </div>
-      
-    <div class="content-button__container container">
-      
-      <a href="./users.php" class="waves-effect btn"><i class="material-icons left">people_outline</i>users manager</a>
-      <a href="./ecom.php" class="waves-effect btn"><i class="material-icons left">shop</i>go to shop</a>
+    <div class="contenthead__container">
+      <img src="assets/images/header-img.svg" class="content-img_1" alt="Front End">
 
+      <div class="content-text__container">
+        <h3>Welcome to Front-End test site...</h3>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit . exahfg Labore molestiae dignissimos asperiores perspiciatis autem consectetur voluptatibus deleniti ex Lorem ipsum dolor sit, amet consectetur adipisicing elitx.</p>
+        <div class="content-button__container container">     
+          <a href="./users.php" class="waves-effect btn"><i class="material-icons left">people_outline</i>users manager</a>
+          <a href="./ecom.php" class="waves-effect btn"><i class="material-icons left">shop</i>go to shop</a>
+        </div>
+      </div>
     </div>
 
     <div class="howwork__container container">
@@ -49,12 +48,12 @@
 
     <div class="goodhand__container">
 
-      <div class="goodhand-title__container">
+      <div class="goodhand-title__container container">
         <span class="goodhand-title1">You’re in a good hand</span>
         <span class="goodhand-title2">There are many variations of passages of Lorem Ipsum available, but the majority have suffered.</span>
       </div>
 
-      <div class="goodhand-content__container">
+      <div class="goodhand-content__container container">
 
         <div class="goodhand-content">
           <div class="goodhand-content_top">
@@ -92,6 +91,29 @@
           </div>
         </div>
 
+      </div>
+
+      <div class="goodhand-logo__container container">
+        <img class="goodhand-logo" src="./assets/images/logo-1.svg" alt="logo">
+        <img class="goodhand-logo" src="./assets/images/logo-2.svg" alt="logo">
+        <img class="goodhand-logo" src="./assets/images/logo-3.svg" alt="logo">
+        <img class="goodhand-logo" src="./assets/images/logo-4.svg" alt="logo">
+        <img class="goodhand-logo" src="./assets/images/logo-5.svg" alt="logo">
+      </div>
+    </div>
+
+    <div class="portfolio__container ">
+      <div class="portfolio-title__container">
+        <span class="portfolio-title1">PORTFOLIO</span>
+        <span class="portfolio-title2">Three Simple Step To Started Working Process</span>
+      </div>
+      <div class="portfolio-content__container">
+        <img src="./assets/images/portfolio-1.jpg" alt="portfolio" class="portfolio-content__img">
+        <img src="./assets/images/portfolio-2.jpg" alt="portfolio" class="portfolio-content__img">
+        <img src="./assets/images/portfolio-3.jpg" alt="portfolio" class="portfolio-content__img">
+        <img src="./assets/images/portfolio-4.jpg" alt="portfolio" class="portfolio-content__img">
+        <img src="./assets/images/portfolio-5.jpg" alt="portfolio" class="portfolio-content__img">
+        <img src="./assets/images/portfolio-6.jpg" alt="portfolio" class="portfolio-content__img">
       </div>
     </div>
 
